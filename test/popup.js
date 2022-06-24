ff064e434b3b57dd37238bdc6e3812522ffa6502
harutiro <harutiro2027@outlook.jp>
2022-06-24 15:48:20 +0900

    update:ボタンを押したら動作をするようにした

---
+++ test/popup.js
@@ -0,0 +1,15 @@
+document.getElementById("btn").addEventListener("click", async () => {
+    //現在のタブのIDを取得する。
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    //タブに対して動作を実行する。
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: onRun,
+    });
+});
+
+function onRun() {
+    //背景の色を変更する。
+    document.body.style.backgroundColor = "#fcc";
+}
